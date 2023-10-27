ba5a89fa55d38fce03c6d78695de2ca2e6d2a70e
DatSung <datsung.dev@gmail.com>
2023-10-27 21:23:37 +0700

    update

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -9,31 +9,63 @@ export default function Question(props) {
 
   return (
     <div className="question-container">
-
       {questions.map((question, i) => (
-
         <div key={question.id} className="question">
           <p>{question.content}</p>
-
           {question.answer.map((answer, j) => (
-
             <div key={answer.id}>
               <input
                 type={question.isMutiple ? "checkbox" : "radio"}
-                name='answerId'
+                name={`answerId-${i}`}
                 value={answer.id}
               />
               {answer.content}
             </div>
-
           ))}
-
         </div>
       ))}
-
     </div>
   );
 }
+
+
+
+// import React, { useEffect, useState } from "react";
+
+// export default function Question(props) {
+//   const [questions, setQuestions] = useState([]);
+
+//   useEffect(() => {
+//     setQuestions(Object.values(props.lsQuizz));
+//   }, [props.lsQuizz]);
+
+//   return (
+//     <div className="question-container">
+
+//       {questions.map((question, i) => (
+
+//         <div key={question.id} className="question">
+//           <p>{question.content}</p>
+
+//           {question.answer.map((answer, j) => (
+
+//             <div key={answer.id}>
+//               <input
+//                 type={question.isMutiple ? "checkbox" : "radio"}
+//                 name='answerId'
+//                 value={answer.id}
+//               />
+//               {answer.content}
+//             </div>
+
+//           ))}
+
+//         </div>
+//       ))}
+
+//     </div>
+//   );
+// }
 
 
 
