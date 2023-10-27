99c20d3c389f4347f67296839a7672f0cdf8ad34
DatSung <datsung.dev@gmail.com>
2023-10-27 21:10:48 +0700

    update form question

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,32 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 export default function Question(props) {
-  const [question, setQuestion] = useState([])
+  const [questions, setQuestions] = useState([]);
+
   useEffect(() => {
-      setQuestion(Object.values(props.lsQuizz))
-  }, [props.lsQuizz])
+    setQuestions(Object.values(props.lsQuizz));
+  }, [props.lsQuizz]);
+
   return (
-    // <div key={question.id} className="question">
-
-    question.map((v, i) => {
-      return (
-        <div key={v.id}>
-          <p>{v.content}</p>
-          {
-            v.answer.map((v,i)=>{
-              return (
-                <>
-                <input key={v.id} type={v.isMultiple ? "checkbox" : "radio"} name="answerId" value={v.id}></input>{v.content}<br/>
-                </>
-              )
-            })
-          }
-          
+    <div className="question-container">
+      {questions.map((question, i) => (
+        <div key={question.id} className="question">
+          <p>{question.content}</p>
+          {question.answer.map((answer, j) => (
+            <div key={answer.id}>
+              <input
+                type={answer.isMultiple ? "checkbox" : "radio"}
+                name={`answerId-${i}`}
+                value={answer.id}
+              />
+              {answer.content}
+            </div>
+          ))}
         </div>
-      )
-    })
-    // </div
-
-
+      ))}
+    </div>
   );
 }
