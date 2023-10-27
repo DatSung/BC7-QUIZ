fd77765c22d7806636e6491ed328e5b78f0170df
DatSung <datsung.dev@gmail.com>
2023-10-27 21:16:01 +0700

    update

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -14,7 +14,7 @@ export default function Question(props) {
 
         <div key={question.id} className="question">
           <p>{question.content}</p>
-          
+
           {question.answer.map((answer, j) => (
 
             <div key={answer.id}>
@@ -25,7 +25,7 @@ export default function Question(props) {
               />
               {answer.content}
             </div>
-            
+
           ))}
 
         </div>
@@ -34,3 +34,38 @@ export default function Question(props) {
     </div>
   );
 }
+
+
+
+// import React, { useEffect, useState } from "react";
+
+// export default function Question(props) {
+//   const [question, setQuestion] = useState([])
+//   useEffect(() => {
+//     setQuestion(Object.values(props.lsQuizz))
+//   }, [props.lsQuizz])
+//   return (
+//     // <div key={question.id} className="question">
+
+//     question.map((v, i) => {
+//       return (
+//         <div key={v.id}>
+//           <p>{v.content}</p>
+//           {
+//             v.answer.map((v, i) => {
+//               return (
+//                 <>
+//                   <input key={v.id} type={v.isMultiple ? "checkbox" : "radio"} name="answerId" value={v.id}></input>{v.content}<br />
+//                 </>
+//               )
+//             })
+//           }
+
+//         </div>
+//       )
+//     })
+//     // </div
+
+
+//   );
+// }
