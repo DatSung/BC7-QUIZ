364e0cc5a0f7ae4c416ccd299627fd00faea5f3c
XuanDat <datnx080903@gmail.com>
2023-10-27 21:04:44 +0700

    update question
    render questions in quiz

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 export default function Question(props) {
-  const [question, setQuestion] = useState(() => {
-    Object.values(props.lsQuizz)
-  })
+  const [question, setQuestion] = useState([])
   useEffect(() => {
-
+      setQuestion(Object.values(props.lsQuizz))
   }, [props.lsQuizz])
   return (
     // <div key={question.id} className="question">
 
     question.map((v, i) => {
       return (
-        <>
-          <input type={question.isMultiple ? "checkbox" : "radio"} name="" value={question.answer}></input>
-        </>
+        <div key={v.id}>
+          <p>{v.content}</p>
+          {
+            v.answer.map((v,i)=>{
+              return (
+                <>
+                <input key={v.id} type={v.isMultiple ? "checkbox" : "radio"} name="answerId" value={v.id}></input>{v.content}<br/>
+                </>
+              )
+            })
+          }
+          
+        </div>
       )
     })
     // </div
