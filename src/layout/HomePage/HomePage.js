48d287b7a5f4bb636e73259bbb4a31112f4791f7
DatSung <datsung.dev@gmail.com>
2023-10-28 07:01:03 +0700

    update lib scss and convert css to scss

--- src/layout/HomePage/HomePage.js
+++ src/layout/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import './HomePage.css';
+import './HomePage.scss';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 
