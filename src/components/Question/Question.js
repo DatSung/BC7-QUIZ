404ee983b217b9f68ec10934a3c8d0965e952105
XuanDat <datnx080903@gmail.com>
2023-10-28 16:01:31 +0700

    check out to the past to reduce stress

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,49 +1,12 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Question(props) {
   const [questions, setQuestions] = useState([]);
   const [answer, setAnswer] = useState({});
 
-  const reducer = (state, action) => {
-    let temp = state.filter(items => items.quesId == action.target)
-    console.log(temp)
-    switch (action.type) {
-      case "CHANGE": {
-
-         
-        break;
-      }
-      case "DELETE": {
-        break;
-      }
-    }
-    return state;
-  };
-
-  const [state, dispatch] = useReducer(reducer, answer);
-
   useEffect(() => {
     setQuestions(Object.values(props.lsQuizz));
-    //setAnswer(Object.keys(props.lsQuizz));
-    setAnswer(
-      Object.keys(props.lsQuizz).map((v) => {
-        return {
-          quesId: v,
-          answerId: []
-        };
-      })
-    );
   }, [props.lsQuizz]);
-
-  const handleAnswerChange = (questionId, answerId, isMultiple) => {
-    questions.map((v, i) => {});
-    if (isMultiple) {
-      // Nếu đây là checkbox, hãy kiểm tra xem đã chọn hay bỏ chọn
-      dispatch({ type: "CHANGE" , data: {answerId : answerId}, target: questionId})
-    } else {
-      // Nếu đây là radio, chỉ cần lưu câu trả lời duy nhất
-    }
-  };
 
   return (
     <div className="question-container">
@@ -57,17 +20,8 @@ export default function Question(props) {
           {question.answer.map((answer, j) => (
             <div key={answer.id}>
               <input
-                onChange={() =>
-                  handleAnswerChange(
-                    question.id,
-                    answer.id,
-                    question.isMultiple
-                  )
-                }
                 type={question.isMutiple ? "checkbox" : "radio"}
-
                 name={`answerId-${i}`}
-
                 value={answer.id}
               />
               {answer.content}
@@ -78,4 +32,3 @@ export default function Question(props) {
     </div>
   );
 }
-
